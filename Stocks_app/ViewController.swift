6d7bde885c881626247c8714d6094542c7b0ae2e
dokdeker <dokdeker@icloud.com>
2022-02-11 04:44:18 +0500

    first step

--- Stocks_app/ViewController.swift
+++ Stocks_app/ViewController.swift
@@ -7,16 +7,101 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    
 
+    @IBOutlet weak var companyNameLabel: UILabel!
+    @IBOutlet weak var currentCompanyNameLabel: UILabel!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var companyPickerView: UIPickerView!
+    
+    //MARK: privat properties
+    private let companies:[String:String] = ["Apple":"AAPL",
+                                             "Microsoft":"MSFT",
+                                             "Google":"GOOG",
+                                             "Amazon":"AMZN",
+                                             "Facebook": "FB"]
+    
+    
+    //MARK: privat methods
+    private func requestQuote(for symbol: String) {
+        let url = URL(string: "https://cloud.iexapis.com/stable/stock/\(symbol)/quote")!
+        let dataTask = URLSession.shared.dataTask(with: url) { data, response, error in
+            guard
+                error == nil,
+                (response as? HTTPURLResponse)?.statusCode == 200,
+                let data = data
+            else {
+                print("Network error")
+                return
+            }
+            
+            self.parseQuote(data: data)
+            
+        }
+        
+        dataTask.resume()
+    }
+    
+    private func parseQuote(data: Data) {
+        do {
+            let jsonObject = try JSONSerialization.jsonObject(with: data)
+            
+            guard
+                let json = jsonObject as? [String:Any],
+                let companyName = json["companyName"] as? String
+            else {
+                print("Invalid Json format")
+                return
+            }
+            
+            print("Company name is \(companyName)")
+            
+        } catch {
+            print("! Json parsing error: " + error.localizedDescription)
+        }
+        
+    }
+    
+    
+    private func displayStockInfo(companyName: String) {
+        self.activityIndicator.stopAnimating()
+        self.companyNameLabel.text = companyName
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.currentCompanyNameLabel.text = "bank"
+        self.companyPickerView.dataSource = self
+        self.companyPickerView.delegate = self
+        
+        self.activityIndicator.startAnimating()
+        self.requestQuote(for: "AAPL")
+        
         
         
     //start
     }
 
-
+    
+    
+    
+    //MARK: UIPickerViewDataSource
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        companies.keys.count
+    }
+    
+    //MARK: UIPickerViewDelegate
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        Array(self.companies.keys)[row]
+    }
+    
 }
 
